bea1bf33fb5d330dbfbc3a1c4eaec2330d66710a
Praveen <praveenk.june9@gmail.com>
2026-02-08 19:09:30 +0530

    Updated app.js

--- App.js
+++ App.js
@@ -1,8 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement("h1", { id: "heading" }, "Namaste ", React.createElement('strong', { style: { color: 'red' } }, 'React '));
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "Namaste ",
+  React.createElement("strong", { style: { color: "red" } }, "React "),
+);
 console.log(heading);
 
+const jsxHeading = <h1 id="heading">Namaste React using JSX</h1>;
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+// root.render(heading);
+root.render(jsxHeading);
